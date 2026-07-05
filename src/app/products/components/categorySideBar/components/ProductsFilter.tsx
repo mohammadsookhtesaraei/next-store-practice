5c04e0693b127d420107fde25b1e29c820c87d9b
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-07-05 22:58:32 +0330

    finish checkbox

--- src/app/products/components/categorySideBar/components/ProductsFilter.tsx
+++ src/app/products/components/categorySideBar/components/ProductsFilter.tsx
@@ -1,6 +1,20 @@
 "use client";
+
+// این کامپوننت یک 
+// Filter Component
+//  در Next.js App Router 
+// است که 
+// وضعیت فیلتر را هم در State
+//  نگه می‌دارد 
+// و هم داخل 
+// URL Query String 
+// ذخیره می‌کند
+// تا با رفرش صفحه یا اشتراک‌گذاری لینک، فیلتر حفظ شود.
+
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
 import { useCallback, useState } from "react";
+
 import CheckBox from "@/components/ui/CheckBox";
 
 import { ICategory } from "@/types/category-interface";
@@ -10,16 +24,21 @@ type ProductsFilterProps = {
 };
 
 const ProductsFilter = ({ categories }: ProductsFilterProps) => {
+  // for set query string in url persist
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
    
+  // state accordion
   const [isOpen, setIsOpen] = useState(false);
 
+  // state checkbox when we selected checkbox value set in state
   const [selectedCategory, setSelectedCategory] = useState<string[]>(
+    // initial state for keep value checkbox in first mount
     searchParams.get("category")?.split(",").filter(Boolean) || []
   );
 
+  // callback for create url and clean query string
   const createQueryString = useCallback(
     (name: string, value: string) => {
       const param = new URLSearchParams(searchParams.toString());
@@ -33,17 +52,20 @@ const ProductsFilter = ({ categories }: ProductsFilterProps) => {
     [searchParams],
   );
 
+  // checkbox change handler
   const changeHanlder = (
     e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
   ) => {
     const { value } = e.target;
 
+    // check value in state
     if (selectedCategory.includes(value)) {
       const newSelectedCategory = selectedCategory.filter(
         (item) => item !== value,
       );
       setSelectedCategory(newSelectedCategory);
-
+      
+      // push to url
       router.push(
         pathname +
           "?" +
@@ -52,7 +74,8 @@ const ProductsFilter = ({ categories }: ProductsFilterProps) => {
     } else {
       const newCategory = [...selectedCategory, value];
       setSelectedCategory(newCategory);
-
+      
+      // push to url
       router.push(
         pathname + "?" + createQueryString("category", newCategory.join(",")),
       );
@@ -81,6 +104,7 @@ const ProductsFilter = ({ categories }: ProductsFilterProps) => {
             value={item.englishTitle}
             name="product-type"
             label={item.title}
+            // this is important since input  with type checkbox tick with checked not value
             checked={selectedCategory.includes(item.englishTitle)}
             changeHanlder={changeHanlder}
           />
